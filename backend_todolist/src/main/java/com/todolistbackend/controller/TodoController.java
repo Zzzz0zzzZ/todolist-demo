ae50df0813512fb09e19836f029a73c9558b8044
Yennefer <1223696764@qq.com>
2022-10-07 16:06:19 +0800

    perf: 代办比例的更新将仅限于对当日任务的操作

--- backend_todolist/src/main/java/com/todolistbackend/controller/TodoController.java
+++ backend_todolist/src/main/java/com/todolistbackend/controller/TodoController.java
@@ -7,36 +7,57 @@ import com.todolistbackend.mapper.TodoMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 @CrossOrigin(origins = {"*", "null"})
 public class TodoController {
     @Autowired
     private TodoMapper todoMapper;
+
     @GetMapping("/todos/{userid}")
     public String getTodos(@PathVariable Integer userid) {
-        QueryWrapper<Todo> qw = new QueryWrapper<>();
-        qw.eq("userid", userid);
-        List<Todo> todos = todoMapper.selectList(qw);
+        QueryWrapper<Todo> wrapper = new QueryWrapper<>();
+        wrapper.eq("userid", userid);
+        List<Todo> todos = todoMapper.selectList(wrapper);
         return JSON.toJSONString(todos);
     }
+
     @PostMapping("/delete")
     public void delTodo(@RequestBody Todo todo) {
         todoMapper.deleteById(todo);
     }
+
     @GetMapping("/count_total/{userid}")
     public long countTodos(@PathVariable Integer userid) {
         QueryWrapper<Todo> wrapper = new QueryWrapper<>();
         wrapper.eq("userid", userid);
-        return todoMapper.selectCount(wrapper);
+        return culTodayTodo(wrapper);
     }
+
     @GetMapping("/count_finish/{userid}")
     public long countFinishTodos(@PathVariable Integer userid) {
         QueryWrapper<Todo> wrapper = new QueryWrapper<>();
         wrapper.eq("status", 1).eq("userid", userid);
-        return todoMapper.selectCount(wrapper);
+        return culTodayTodo(wrapper);
     }
+
+    private long culTodayTodo(QueryWrapper<Todo> wrapper) {
+        Date date = new Date();
+        SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd");
+        List<Todo> todos = todoMapper.selectList(wrapper);
+        long res = 0;
+        for (Todo todo : todos) {
+            if (todo.getDeadline() == null || todo.getDeadline().equals(df.format(date))) {
+                res++;
+            }
+        }
+        return res;
+    }
+
     @PostMapping("/add")
     public void addTodo(@RequestBody Todo todo) {
         todoMapper.insert(todo);
