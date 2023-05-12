5decbc48a913d089e2186680f27a0909391e0028
wsz <1175690167@qq.com>
2023-05-12 13:40:34 +0800

    feat: api接口

--- frontend_todolist/src/main.js
+++ frontend_todolist/src/main.js
@@ -7,8 +7,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
-createApp(App).use(router).use(pinia).use(ElementPlus).use(BaiduCalendar).mount('#app')
+const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
+app.use(router).use(pinia).use(ElementPlus).use(BaiduCalendar).mount('#app')
 
 window.addEventListener('storage', function (e) {
     localStorage.setItem(e.key, e.oldValue)
