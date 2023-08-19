445784b3d35997583f2d585ddc8f032b42e7e844
Yennefer <1223696764@qq.com>
2023-08-20 00:39:52 +0800

    fix: 更改redis使用位置

--- backend_todolist/src/main/java/com/todolistbackend/controller/PhotoController.java
+++ backend_todolist/src/main/java/com/todolistbackend/controller/PhotoController.java
@@ -3,24 +3,17 @@ package com.todolistbackend.controller;
 import com.todolistbackend.service.PhotoService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.annotation.Resource;
 import java.io.IOException;
-import java.util.Arrays;
 import java.util.Map;
-import java.util.concurrent.TimeUnit;
 
 @RestController
 @Slf4j
 public class PhotoController {
     @Autowired
     private PhotoService photoService;
-
-    @Resource
-    private RedisTemplate<String, Object> redisTemplate;
 
     @PostMapping("/setphoto")
     public void setPhoto(MultipartFile file, Integer userid, @RequestHeader Map<String, String> head) throws IOException {
@@ -29,16 +22,6 @@ public class PhotoController {
 
     @GetMapping("/getphoto/{userid}")
     public byte[] getPhoto(@PathVariable Integer userid, @RequestHeader Map<String, String> head) {
-        // 从redis获取photo
-        byte[] hj = (byte[]) redisTemplate.opsForValue().get("hj");
-        // 判断是否在redis
-        if (hj != null) {
-            log.warn("hj", hj);
-            return hj;
-        }
-        byte[] photo = photoService.getPhoto(userid);
-        // 设置过期时间
-        redisTemplate.opsForValue().set("hj", photo, 3, TimeUnit.DAYS);
-        return photo;
+        return photoService.getPhoto(userid);
     }
 }
--- backend_todolist/src/main/java/com/todolistbackend/service/impl/PhotoServiceImpl.java
+++ backend_todolist/src/main/java/com/todolistbackend/service/impl/PhotoServiceImpl.java
@@ -6,29 +6,46 @@ import com.todolistbackend.entity.Photo;
 import com.todolistbackend.mapper.PhotoMapper;
 import com.todolistbackend.service.PhotoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.annotation.Resource;
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 @Service
 public class PhotoServiceImpl implements PhotoService {
     @Autowired
     private PhotoMapper photoMapper;
 
+    @Resource
+    private RedisTemplate<String, Object> redisTemplate;
+
+    private final String photoRedisBaseKey = "todo_photo_";
     @Override
     public void setPhoto(MultipartFile file, Integer userid) throws IOException {
         byte[] bytes = file.getBytes();
         UpdateWrapper<Photo> updateWrapper = new UpdateWrapper<>();
         updateWrapper.eq("userid", userid).set("data", bytes);
         photoMapper.update(new Photo(), updateWrapper);
+        redisTemplate.opsForValue().set(photoRedisBaseKey+userid, bytes, 3, TimeUnit.DAYS);
     }
 
     @Override
     public byte[] getPhoto(Integer userid) {
+        String photoRedisKey = photoRedisBaseKey+userid;
+        // 从redis获取photo
+        byte[] photo = (byte[]) redisTemplate.opsForValue().get(photoRedisKey);
+        // 判断是否在redis
+        if (photo != null) {
+            return photo;
+        }
+        // 设置过期时间
         QueryWrapper<Photo> photoQueryWrapper = new QueryWrapper<>();
         photoQueryWrapper.eq("userid", userid);
-        Photo photo = photoMapper.selectOne(photoQueryWrapper);
-        return photo.getData();
+        photo = photoMapper.selectOne(photoQueryWrapper).getData();
+        redisTemplate.opsForValue().set(photoRedisKey, photo, 3, TimeUnit.DAYS);
+        return photo;
     }
 }
