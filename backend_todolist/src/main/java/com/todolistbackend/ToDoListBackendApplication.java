d56bcaf195c583920a1a5476ebad53c15aefc6ec
wsz <1175690167@qq.com>
2023-08-19 22:57:40 +0800

    chore: 修改yml配置

--- backend_todolist/src/main/java/com/todolistbackend/ToDoListBackendApplication.java
+++ backend_todolist/src/main/java/com/todolistbackend/ToDoListBackendApplication.java
@@ -1,13 +1,16 @@
 package com.todolistbackend;
 
+import jdk.nashorn.internal.objects.annotations.Property;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling//开启定时
 @MapperScan("com.todolistbackend.mapper")
+@PropertySource("classpath:secrets.txt")
 public class ToDoListBackendApplication {
     public static void main(String[] args) {
         SpringApplication.run(ToDoListBackendApplication.class, args);
