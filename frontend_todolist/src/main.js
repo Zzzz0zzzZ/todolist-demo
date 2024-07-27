ca5d7968e7a64dde2b53e455751ad725c7857e83
teash1rt <1223696764@qq.com>
2024-07-27 13:39:55 +0800

    feat: 打包产物优化

--- frontend_todolist/src/main.js
+++ frontend_todolist/src/main.js
@@ -2,19 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import pinia from './stores/store'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap/dist/js/bootstrap'
 import './index.css'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component)
-}
-app.use(router).use(pinia).use(ElementPlus).mount('#app')
 
-window.addEventListener('storage', function (e) {
-  localStorage.setItem(e.key, e.oldValue)
-})
+app.use(router).use(pinia).mount('#app')
