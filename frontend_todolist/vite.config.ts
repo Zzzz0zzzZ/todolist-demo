780ece442b0626a449749ee2b422a99253ae2b1a
TeaShirt <95997368+teash1rt@users.noreply.github.com>
2024-08-09 00:18:55 +0800

    feat: vite环境配置 (#11)
    * feat: vite环境配置
    
    * docs: update changelog

--- frontend_todolist/vite.config.ts
+++ frontend_todolist/vite.config.ts
@@ -1,48 +1,51 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 
-export default defineConfig({
-  plugins: [
-    vue(),
-    AutoImport({
-      resolvers: [ElementPlusResolver()]
-    }),
-    Components({
-      resolvers: [ElementPlusResolver()]
-    })
-  ],
-  server: {
-    port: 9974,
-    proxy: {
-      '/api/': {
-        target: 'http://127.0.0.1:8060/',
-        rewrite: (path) => path.replace(/^\/api/, ''),
-        changeOrigin: true,
-        ws: true
+export default defineConfig(({ mode }) => {
+  const target = loadEnv(mode, process.cwd()).VITE_API_TARGET
+  return {
+    plugins: [
+      vue(),
+      AutoImport({
+        resolvers: [ElementPlusResolver()]
+      }),
+      Components({
+        resolvers: [ElementPlusResolver()]
+      })
+    ],
+    server: {
+      port: 9974,
+      proxy: {
+        '/api/': {
+          target,
+          rewrite: (path) => path.replace(/^\/api/, ''),
+          changeOrigin: true,
+          ws: true
+        }
       }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': '/src'
-    }
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          vue: ['vue', 'vue-router', 'pinia'],
-          utils: ['axios', 'dayjs'],
-          ui: ['element-plus', '@element-plus/icons-vue']
-        },
-        assetFileNames: (chunk) => {
-          if (/\.(png|jpg)$/.test(chunk.name)) {
-            return 'imgs/[name][extname]'
+    },
+    resolve: {
+      alias: {
+        '@': '/src'
+      }
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            vue: ['vue', 'vue-router', 'pinia'],
+            utils: ['axios', 'dayjs'],
+            ui: ['element-plus', '@element-plus/icons-vue']
+          },
+          assetFileNames: (chunk) => {
+            if (/\.(png|jpg)$/.test(chunk.name)) {
+              return 'imgs/[name][extname]'
+            }
+            return 'assets/[name]-[hash][extname]'
           }
-          return 'assets/[name]-[hash][extname]'
         }
       }
     }
